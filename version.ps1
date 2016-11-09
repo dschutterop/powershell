2a9dbbc47a11e9a025afd008417266fe7be1607a
Daniel Schutterop <daniel@schutterop.nl>
2016-11-09 09:42:49 +0100

    Initial commit

---
+++ version.ps1
@@ -0,0 +1,9 @@
+#
+# This is a dummy so I can test grabbing a version
+# number from Powershell to check if the script
+# should update itself.
+#
+
+# I'll adhere to the Semantic Versioning 2.0.0 standard:
+# major.minor.patch
+$scriptVersion = '0.1.0'
