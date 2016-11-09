ccb87bfd18f0238e60431a6faaf1411a4993e3b9
Daniel Schutterop <daniel@schutterop.nl>
2016-11-09 11:12:54 +0100

    Update version.ps1

--- version.ps1
+++ version.ps1
@@ -7,3 +7,12 @@
 # I'll adhere to the Semantic Versioning 2.0.0 standard:
 # major.minor.patch
 $scriptVersion = '0.1.0'
+
+
+function script:showVersion {
+  # This function writes the current version so we can
+  # do some funky version checking
+  #
+  param ([string]$thisVersion = $scriptVersion) 
+  write-host $thisVersion
+}
